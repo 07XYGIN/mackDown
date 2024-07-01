4dcf02139188b8eadfcf97f59252dca095e68a01
乔宇帅 <>
2024-07-01 12:00:21 +0800

    'updata'

---
+++ Vue/vite-project/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <input type="text" v-model="inputValue" @focus="handleFocus" @blur="handleBlur" @keydown="handleKeyDown" />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            inputValue: '',
+        };
+    },
+    methods: {
+        handleFocus() {
+            // 在这里，你可以执行与键盘事件类似的代码
+            // 例如，更新某个数据或执行某个函数
+            console.log('Input is focused');
+            // 假设你有一个与键盘事件相关的函数
+            this.keyboardRelatedFunction();
+        },
+        handleBlur() {
+            // 同样，你可以在这里执行与键盘事件类似的代码
+            console.log('Input is blurred');
+            // 再次调用与键盘事件相关的函数
+            this.keyboardRelatedFunction();
+        },
+        handleKeyDown(event) {
+            // 处理键盘事件
+            console.log('Keydown event:', event.key);
+        },
+        keyboardRelatedFunction() {
+            // 这是一个与键盘事件相关的函数
+            // 你可以在这里放置任何与键盘输入相关的逻辑
+            console.log('Executing keyboard-related function');
+        },
+    },
+};
+</script>
